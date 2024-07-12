80f7bc127e0305d512a7215df6f2d721bc49b3cc
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-07-12 13:50:57 -0300

    Update file.js

--- Tela de login/file.js
+++ Tela de login/file.js
@@ -1,15 +1,21 @@
 function Enviar(){
 
     
-        let emailUsuario = document.getElementById("Email").value
-        let senhaUsuario = document.getElementById("Senha").value
-        let sexoUsuario = document.querySelector("input[name='sexo']:checked").value
-    
+    let emailUsuario = document.getElementById("Email").value
+    let senhaUsuario = document.getElementById("Senha").value
+    let sexoUsuario = document.querySelector("input[name='sexo']:checked").value
+    let Termos = document.getElementById("Termos")
+
+    if(Termos.checked){
         alert(`Seu email é: ${emailUsuario}\n Sua senha é: ${senhaUsuario}\nSeu genero é ${sexoUsuario}`)
-    
-    
-    
-     window.location.href='index.html'
-    
+
+        window.location.href='index.html'
+    }
+    else{
+        alert("Aceite os termos")
+    }
+
+
+
+
 }
-    
